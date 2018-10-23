ae922984ba04d0fd1178c3b3643f120347797939
GitHubAlexTroy <podliza123@gmail.com>
2018-10-23 17:43:37 +0300

    added the Singl to Component

--- app/src/main/java/com/alextroy/testdagger/di/components/AppComponent.java
+++ app/src/main/java/com/alextroy/testdagger/di/components/AppComponent.java
@@ -5,9 +5,12 @@ import com.alextroy.testdagger.view.MainActivity;
 import com.alextroy.testdagger.di.modules.NetworkModule;
 import com.alextroy.testdagger.di.modules.StorageModule;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 @Component(modules = {AppModule.class, StorageModule.class, NetworkModule.class})
+@Singleton
 public interface AppComponent {
     void inject(MainActivity mainActivity);
 }
